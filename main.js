c4403ef7a42a3c7cb6676b8163409bbb3379f7f6
DenisovAndrey <disik43r@gmail.com>
2019-01-02 00:28:03 +0300

    add lvls and persons

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 $(function(){
-	function game(){
+	var diff = 'easy',
+			pers = 'bird';
 
+	function game(diff, pers){
+		var lvl;
+		switch(diff) {
+			case 'easy':
+				lvl = 1;
+				break;
+			case 'normal':
+				lvl = 2;
+				break;
+			case 'hard':
+				lvl = 3;
+				break;
+			case 'imp':
+				lvl = 4;
+				break;
+		}
 		var cvs = document.getElementById('canvas');
 		var ctx = cvs.getContext('2d');
 
@@ -10,7 +27,7 @@ $(function(){
 		var pipeUp = new Image();
 		var pipeDown = new Image();
 
-		bird.src = "images/flappy_bird_bird.png"
+		bird.src = "images/bird_" + pers + ".png"
 		bg.src = "images/flappy_bird_bg.png"
 		fg.src = "images/flappy_bird_fg.png"
 		pipeUp.src = "images/flappy_bird_pipeUp.png"
@@ -47,9 +64,9 @@ $(function(){
 				ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
 				ctx.drawImage(pipeDown, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
-				pipe[i].x--;
+				pipe[i].x = pipe[i].x - lvl;
 
-				if(pipe[i].x == 85){
+				if(pipe[i].x == 84){
 					pipe.push({
 						x: cvs.width,
 						y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
@@ -59,12 +76,15 @@ $(function(){
 					&& xPos <= pipe[i].x + pipeUp.width
 					&& (yPos <= pipe[i].y + pipeUp.height
 					|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-					location.reload(); // Перезагрузка страницы
+					$('canvas').addClass('d-none');
+					$('.mainMenu').removeClass('d-none');
+					$('.mainMenu__menu').removeClass('d-none');
+					requestAnimationFrame(null);
 				};
-				if(pipe[i].x == 5) {
+				if(pipe[i].x == lvl) {
 					score++;
 					score_audio.play();
-				}
+				};
 
 			};
 			
@@ -75,10 +95,11 @@ $(function(){
 			requestAnimationFrame(draw);
 			ctx.fillStyle = "#000";
 			ctx.font = "24px Verdana";
-			ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+			ctx.fillText("Score: " + score, 10, cvs.height - 20);
 		};
 		pipeDown.onload = draw;
 	};
+
 	$('.mainMenu__menu button').click(function() {
 		var option = $(this).attr('data');
 		$('.mainMenu__container').addClass('d-none');
@@ -91,9 +112,24 @@ $(function(){
 				break;
 			case 'start':
 				$('.mainMenu').addClass('d-none');
-				$('canvas').removeClass('d-none')
-				game();
+				$('canvas').removeClass('d-none');
+				game(diff, pers);
 				break;
 		}
 	});
+	$('button[data="mainmenu"]').click(function(){
+		$('.mainMenu__container').addClass('d-none');
+		$('.mainMenu__menu').removeClass('d-none');
+	});
+	$('.mainMenu__person button:not("#mainmenu")').click(function(event) {
+		pers = $(this).attr('data');
+		$('.mainMenu__person button').removeClass('mainMenu__person_active');
+		$(this).addClass('mainMenu__person_active');
+	});
+	$('.mainMenu__diff button:not("#mainmenu")').click(function(event) {
+		diff = $(this).attr('data');
+		$('.mainMenu__diff button').removeClass('mainMenu__person_active');
+		$(this).addClass('mainMenu__person_active');
+	});
+
 });
